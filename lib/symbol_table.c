ef815989bdfe74b8ea403ca58c6e515437b85db2
Fernanda Gonçalves Barros <nandagb23@gmail.com>
2025-11-24 22:36:48 -0300

    adds access and update functions to symbol table

--- lib/symbol_table.c
+++ lib/symbol_table.c
@@ -33,3 +33,151 @@ void table_destroy(table* table) {
     free(table->entries);
     free(table);
 }
+
+#define FNV_OFFSET 14695981039346656037UL
+#define FNV_PRIME 1099511628211UL
+
+// Return 64-bit FNV-1a hash for key (NUL-terminated). See description:
+// https://en.wikipedia.org/wiki/Fowler–Noll–Vo_hash_function
+static uint64_t hash_key(const char* key) {
+    uint64_t hash = FNV_OFFSET;
+    for (const char* p = key; *p; p++) {
+        hash ^= (uint64_t)(unsigned char)(*p);
+        hash *= FNV_PRIME;
+    }
+    return hash;
+}
+
+table_entry* get_table_entry(table_entry* entries, int capacity, const char* key, uint64_t hash, int index) {
+    /* if null then key is not in the table */
+    while (entries[index].key != NULL) {
+        if (strcmp(key, entries[index].key) == 0) {
+            /* Found key, return value. */
+            return &entries[index];
+        }
+
+        /* either a collision or key isnt in the table */
+        index++;
+
+        if (index >= capacity) {
+            /* reset index */
+            index = 0;
+        }
+    }
+    return NULL;
+}
+
+type table_get_type(table* table, const char* key) {
+    uint64_t hash = hash_key(key);
+    int index = hash % table->capacity;
+
+    table_entry* entry = get_table_entry(table->entries, table->capacity, key, hash, index);
+
+    if (entry == NULL) {
+        return UNDEFINED_TYPE;
+    }
+
+    return entry->type;
+}
+
+structure table_get_structure(table* table, const char* key) {
+    uint64_t hash = hash_key(key);
+    int index = hash % table->capacity;
+
+    table_entry* entry = get_table_entry(table->entries, table->capacity, key, hash, index);
+
+    if (entry == NULL) {
+        return UNDEFINED_STRUCTURE;
+    }
+
+    return entry->structure;
+}
+
+table_entry* table_get_entry_object(table* table, const char* key) {
+    uint64_t hash = hash_key(key);
+    int index = hash % table->capacity;
+
+    table_entry* entry = get_table_entry(table->entries, table->capacity, key, hash, index);
+
+    if (entry == NULL) {
+        return NULL;
+    }
+
+    return entry;
+}
+
+static const char* table_set_entry(table_entry* entries, int capacity, const char* key, type type, structure structure, int* plength) {
+    // AND hash with capacity-1 to ensure it's within entries array.
+    uint64_t hash = hash_key(key);
+    int index = hash % capacity;
+
+    table_entry* entry = get_table_entry(entries, capacity, key, hash, index);
+    if (entry != NULL) {
+        entry->type = type;
+        entry->structure = structure;
+        return entry->key;
+    }
+
+    /* key doesnt exists yet, allocate and if needed, then insert it. */
+    if (plength != NULL) {
+        key = strdup(key);
+
+        if (key == NULL) {
+            return NULL;
+        }
+
+        (*plength)++;
+    }
+
+    entries[index].key = (char*)key;
+    entries[index].type = type;
+    entries[index].structure = structure;
+
+    return key;
+}
+
+const char* table_set(table* table, const char* key, type type, structure structure) {
+    /* if length will exceed half of current capacity, expand it. */
+    if (table->length >= table->capacity / 2) {
+        if (!table_expand(table)) {
+            return NULL;
+        }
+    }
+
+    return table_set_entry(table->entries, table->capacity, key, type, structure, &table->length);
+}
+
+/* expand hash table to twice its current size. Return true on success, false if out of memory*/
+static bool table_expand(table* table) {
+    /* allocate new entries array */
+    int new_capacity = table->capacity * 2;
+
+    if (new_capacity < table->capacity) {
+        /* overflow (capacity would be too big) */
+        return false;
+    }
+
+    table_entry* new_entries = calloc(new_capacity, sizeof(table_entry));
+
+    if (new_entries == NULL) {
+        return false;
+    }
+
+    /* iterate entries, move all non-empty ones to new table's entries */
+    for (int i = 0; i < table->capacity; i++) {
+        table_entry entry = table->entries[i];
+        if (entry.key != NULL) {
+            table_set_entry(new_entries, new_capacity, entry.key, entry.type, entry.structure, NULL);
+        }
+    }
+
+    /* Free old entries array and update this table's details */
+    free(table->entries);
+    table->entries = new_entries;
+    table->capacity = new_capacity;
+    return true;
+}
+
+
+
+
--- lib/symbol_table.h
+++ lib/symbol_table.h
@@ -2,9 +2,12 @@
 #define SYMBOLTABLE
 
 #include "record.h"
+#include <stdint.h>   /* uint64_t */
+#include <stddef.h>   /* size_t */
+#include <stdbool.h>  /* bool */
 
 typedef enum { 
-	ELIST, ESTRUCT, EPRIMARY
+	ELIST, ESTRUCT, EPRIMARY, UNDEFINED_STRUCTURE
 } structure;
 
 typedef struct {
@@ -16,12 +19,18 @@ typedef struct {
 
 typedef struct {
     table_entry* entries;  /* array of table entries */
-    int capacity;       /* actual size of entries array*/
-    int length;         /* number of items currently in the hash table */
+    int capacity;          /* actual size of entries array*/
+    int length;            /* number of items currently in the hash table */
 } table;
 
 table* table_create(void);
 
 void table_destroy(table* table);
+
+void* table_get(table* table, const char* key);
+
+const char* table_set(table* table, const char* key, type type, structure structure);
+
+static bool table_expand(table* table);
 
 #endif
