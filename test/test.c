752a48654f415ca9899ee1e0e29405d575559ebc
marcos-arthur <marcos.marcos.arthur291@gmail.com>
2025-11-25 15:58:50 -0300

    Added stack

--- test/test.c
+++ test/test.c
@@ -4,11 +4,31 @@
 #include "./../lib/record.h"
 #include "./../lib/file_gen.h"
 #include "./../lib/symbol_table.h"
+#include "./../lib/scope.h"
 
 int main(){
-    printf("TESTING SYMBOL TABLE\n");
-    table* sym_table = table_create();
-    printf("SYMBOL TABLE CREATED\n");
-    table_entry* entry = malloc(sizeof(table_entry));
+    // printf("TESTING SYMBOL TABLE\n");
+    // table* sym_table = table_create();
+    // printf("SYMBOL TABLE CREATED\n");
+    // table_entry* entry = malloc(sizeof(table_entry));
+
+    Stack stack;
+    initialize(&stack);  
+
+    push(&stack, "global");
+    printf("Top element: %s\n", peek(&stack));
+
+    push(&stack, "main");
+    printf("Top element: %s\n", peek(&stack));
+
+    push(&stack, "if1");
+    printf("Top element: %s\n", peek(&stack));
+
+    push(&stack, "if2");
+    printf("Top element: %s\n", peek(&stack));
+
+    pop(&stack);
+    printf("Peeked element at position %d: %s\n", 1, peek_position(&stack, 1));
+
     return 0;
 }
